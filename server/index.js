70ac52d9a5d0793e203947a8d95ba8e6219fadf0
songerard <songerard@gmail.com>
2022-03-05 11:08:53 +0800

    Add join in msg and add user info for each message

--- server/index.js
+++ server/index.js
@@ -3,6 +3,15 @@ const { Server } = require("socket.io")
 module.exports = (server) => {
   const io = new Server(server)
   io.on('connection', (socket) => {
+
+    socket.on('user connected', (user) => {
+      const data = {
+        ...user,
+        joinedAt: new Date()
+      }
+      io.emit('user connected', data)
+    })
+
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg)
     })

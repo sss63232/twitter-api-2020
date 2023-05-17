87045707591bf53b19c5f22682ea0a1ad3123b9b
yu-hsin.chen <yu-hsin.chen@yu-hsin.local>
2023-05-17 10:27:24 +0900

    Refactor models/index.js to use environment variables for database configuration instead of relying on a config file

--- models/index.js
+++ models/index.js
@@ -5,17 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const envConfig = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  console.log('TCL=> ~ process.env:', process.env)
-  console.log('TCL=> ~ config.use_env_variable:', config.use_env_variable)
-
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (!!envConfig.use_env_variable) {
+  sequelize = new Sequelize(process.env[envConfig.use_env_variable], envConfig);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(envConfig.database, envConfig.username, envConfig.password, envConfig);
 }
 
 fs
